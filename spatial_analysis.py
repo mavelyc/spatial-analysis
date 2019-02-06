4d22e526a73afe2e2324f437b6774e1c31c90e0a
mavelyc <mavelyc@mcmaster.ca>
2019-02-06 14:43:13 -0500

    structure base working

--- spatial_analysis.py
+++ spatial_analysis.py
@@ -1,37 +1,54 @@
 # import matplotlib.pyplot as plt
-
 # plt.plot([1,2,3],[5,7,4])
-
 # plt.show()
 
 from shapely.geometry import Polygon
 from shapely.geometry import MultiPolygon
+from ast import literal_eval
 
-polygon = Polygon([(0,0),(1,1),(2,1),(1,0)])
-print (polygon.area)
-print (polygon.length)
-
-polygon2 = Polygon([(1,1),(2,1),(1,0)])
-print (polygon2.area)
-print (polygon2.length)
-
-polygons = MultiPolygon([polygon,polygon2])
-
-print (polygons.bounds)
+# list1 = literal_eval(input("Enter it: "))
+# polygon = Polygon(list1)
+# print (polygon.area)
+# print (polygon.length)
+# print (type(list1))
 
 #object.contains(other)  This returns True if no points of other lie in the exterior of the object and at least one point of the interior of other lies in the interior of object
 #object.within(other)  This is the same as object.contains but only returns True if the obeject within intersects ONLY with interior not its boundary
 
-print(polygon.contains(polygon2))
+# print(polygon.contains(polygon2))
+
+class Room:
+    def __init__(self, shape):
+        self.shape = shape
+
+    def area(self):
+        tmp = Polygon(self.shape)
+        print (tmp.area)
 
 class Unit:
-    def __init__(self, name):
-        shape = input("Enter the coordinates of the room [(0,0),(1,,1)...]:")
-        self.name = name
+    def __init__(self, number, shape):
+        self.number = number
         self.shape = shape
-        print(self.name)
-        print(self.shape)
 
-test = Unit("bruv")
-test1 = Unit("Oi")
+numberOfChillers = 0
+numberOfBoilers = 0
+shapeOfRoom = 0
+def howManyUnits():
+    global numberOfChillers, numberOfBoilers, shapeOfRoom
+    shapeOfRoom = literal_eval(input("Enter the coordinates of the room [(0,0),(1,1)...]: "))
+    numberOfChillers = input("Enter the number of chillers: ")
+    numberOfBoilers = input("Enter the number of boilers: ")
+
+
+
+
+
+def main():
+    howManyUnits()
+    room = Room(shapeOfRoom)
+    room.area()
+
+    
+if __name__ == '__main__':
+    main()
 
