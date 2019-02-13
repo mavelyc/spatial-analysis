904c20fffa3d5c223c24fae4afc5564ac3a81a14
mavelyc <mavelyc@mcmaster.ca>
2019-02-13 11:57:05 -0500

    able to scan through first column, taking each individual tuple

--- xlrd_test.py
+++ xlrd_test.py
@@ -4,7 +4,7 @@ excel_file = 'Dimensions.xlsx'
 wb = xlrd.open_workbook(excel_file)
 sheet = wb.sheet_by_index(0)
 
-print(sheet.cell_value(0,0))
-
+for i in range(1,sheet.nrows):
+    print (sheet.cell_value(i,0))
 # print ("Column headings:")
 # print (df.columns)
